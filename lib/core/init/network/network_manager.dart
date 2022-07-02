a1f01859276a4c1dd680894ee8404682251bc60a
justBeHonest <ahmethilmiask@gmail.com>
2022-07-02 23:40:48 +0300

    chore: network manager added

--- lib/core/init/network/network_manager.dart
+++ lib/core/init/network/network_manager.dart
@@ -5,7 +5,6 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 
 // Project imports:
-import '../../base/model/base_model.dart';
 import '../../constants/enums/preferences_keys.dart';
 import '../cache/locale_manager.dart';
 
@@ -41,19 +40,19 @@ class NetworkManager {
 
   late Dio _dio;
 
-  Future dioGet<T extends BaseModel>(String path, T model) async {
-    final response = await _dio.get(path);
-    switch (response.statusCode) {
-      case HttpStatus.ok:
-        final responseBody = response.data;
-        if (responseBody is List) {
-          return responseBody.map((e) => model.fromJson(e)).toList();
-        } else if (responseBody is Map) {
-          return model.fromJson(responseBody as Map<String, Object>);
-        }
-        return responseBody;
-        break;
-      default:
-    }
-  }
+  //Future dioGet<T extends BaseModel>(String path, T model) async {
+  //  final response = await _dio.get(path);
+  //  switch (response.statusCode) {
+  //    case HttpStatus.ok:
+  //      final responseBody = response.data;
+  //      if (responseBody is List) {
+  //        return responseBody.map((e) => model.fromJson(e)).toList();
+  //      } else if (responseBody is Map) {
+  //        return model.fromJson(responseBody as Map<String, Object>);
+  //      }
+  //      return responseBody;
+  //      break;
+  //    default:
+  //  }
+  //}
 }
